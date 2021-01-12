ecbf238e2be785c703ad8cedebd69dc5b94742fd
BiXtico <56024802+BiXtico@users.noreply.github.com>
2021-01-12 02:18:52 +0200

    new modificaitons

--- AMSServer/src/AMS/SystemManager.java
+++ AMSServer/src/AMS/SystemManager.java
@@ -1,18 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package AMS;
 
 import AMS.Interfaces.SystemManagerInterface;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-/**
- *
- * @author mahmo
- */
+
 public class SystemManager implements SystemManagerInterface {
 
     public SystemManager() throws RemoteException {
